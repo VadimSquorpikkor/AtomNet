716732c3a9f63f9619580d26c352159d03487861
VadimSquorpikkor <uniqueehub1024>
2018-08-05 14:42:24 +0300

    home 18

--- bdkg22/script.js
+++ bdkg22/script.js
@@ -1,13 +1,14 @@
 function get_k() {
-    var a = parseFloat(document.getElementById('a').value);
-    var b = parseInt(document.getElementById('b').value);
-    var c = parseInt(document.getElementById('c').value);
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseInt(document.getElementById('b').value);
+    let c = parseInt(document.getElementById('c').value);
+    //Почему let вместо? var
 
-/*
-    if (isNaN(a)==true) a=0;
-    if (isNaN(b)==true) b=0;
-*/
+    /*
+        if (isNaN(a)==true) a=0;
+        if (isNaN(b)==true) b=0;
+    */
 
 
-    document.getElementById('result').value = (a - b/(2.6 * c)) * 100;
+    document.getElementById('result').value = (a - b / (2.6 * c)) * 100;
 }
