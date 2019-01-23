c51488281398754adce925d1cc79f889e7e2a908
VadimSquorpikkor <vadimserikov11@gmail.com>
2019-01-23 12:20:38 +0300

    work 90

--- 1103/script_r.js
+++ 1103/script_r.js
@@ -2,3 +2,8 @@ function get_r() {
     let a = parseFloat(document.getElementById('a').value);
     document.getElementById('result').value = 47/a*1090;
 }
+
+function get_sm() {
+    let b = parseFloat(document.getElementById('b').value);
+    document.getElementById('res_sm').value = 5.5-2.6*b/3.45;
+}
