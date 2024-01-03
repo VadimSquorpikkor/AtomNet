22a6c3e83cd39aa62f35f95d7c03934033d7d69a
VadimSquorpikkor <uniqueehub1024>
2024-01-03 16:47:12 +0300

    03.01.24 1. AtomtexLibrary 3.75

--- include.js
+++ include.js
@@ -313,7 +313,7 @@ function getRegMenu(mode, id) {
         '        <li><a href="../doc_macro/macro.html">Макрос для протоколов и сертификатов</a>' +
         '            <div>' +
         '                <ul>' +
-        '                   <li><a href="../docs/AtomtexLibrary.zip">Скачать макрос - 3.69</a></li>' +
+        '                   <li><a href="../docs/AtomtexLibrary.zip">Скачать макрос - 3.75</a></li>' +
         '                </ul>' +
         '            </div>' +
         '        </li>' +
